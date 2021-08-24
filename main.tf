9c2a29aa649b68743021928c8e5f281de1ba6439
jagan <jagan.mcc@gmail.com>
2021-08-24 11:35:35 -0400

    testing gh action 0824 2

--- main.tf
+++ main.tf
@@ -1,11 +1,33 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-aws-crc"
+#     key    = "crc/tfstate/terraform.tfstate"
+#     region = "us-east-2"
+#   }
+# }
+
+
 terraform {
-  backend "s3" {
-    bucket = "terraform-aws-crc"
-    key    = "crc/tfstate/terraform.tfstate"
-    region = "us-east-2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 0.14"
+
+  backend "remote" {
+    organization = "jag010"
+
+    workspaces {
+      name = "gh-actions"
+    }
   }
 }
-
 
 module "s3-website" {
   source      = "./modules/s3-website"
