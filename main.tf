8ac3d2a4e1e3cbaa5a118de1982db40109f81001
jagan <jagan.mcc@gmail.com>
2021-08-24 00:31:43 -0400

    commented s3 backend

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-aws-crc"
-    key    = "crc/tfstate/terraform.tfstate"
-    region = "us-east-2"
-  }
-}
-
-
-# module "s3-website" {
-#   source      = "./modules/s3-website"
-#   domain_name = var.site_name
-#   # webpath     = "/Users/jag/Documents/CloudResumeChallenge/aws-crc/HTML-Webpage"
-#   webpath = "./HTML-Webpage"
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-aws-crc"
+#     key    = "crc/tfstate/terraform.tfstate"
+#     region = "us-east-2"
+#   }
 # }
+
+
+module "s3-website" {
+  source      = "./modules/s3-website"
+  domain_name = var.site_name
+  # webpath     = "/Users/jag/Documents/CloudResumeChallenge/aws-crc/HTML-Webpage"
+  webpath = "./HTML-Webpage"
+}
 
 
 # output "website_endpoint" {
