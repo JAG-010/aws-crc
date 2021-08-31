c50109d555437ab6da943e402bca32e021e8ffc5
jagan <jagan.mcc@gmail.com>
2021-08-30 22:49:33 -0400

    removed geo-restriction

--- modules/s3-website/cloudfront.tf
+++ modules/s3-website/cloudfront.tf
@@ -94,8 +94,8 @@ resource "aws_cloudfront_distribution" "cdn" {
 
   restrictions {
     geo_restriction {
-      restriction_type = "whitelist"
-      locations        = ["US", "CA", "GB", "DE"]
+      restriction_type = "none"
+      # locations        = ["US", "CA", "GB", "DE"]
     }
   }
 
