08d0e6621e908e3ce7e47be2e7e4602abe42b923
jagan <jagan.mcc@gmail.com>
2021-08-31 13:24:38 -0400

    delted old tf files

--- temp-terraform/local.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  tags = {
-    Name = "aws-crc"
-  }
-}
--- temp-terraform/variable.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_region" {
-  default = "us-east-1"
-}
-
-variable "site_name" {
-  type    = string
-  default = "jagan-sekaran.me"
-}
