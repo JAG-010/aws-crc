6f3b3a43e6715a3bf3aef00d59f258132e11c8a3
jagan <jagan.mcc@gmail.com>
2021-08-31 21:20:54 -0400

    code cleanup

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "terraform-aws-crc"
-#     key    = "crc/tfstate/terraform.tfstate"
-#     region = "us-east-2"
-#   }
-# }
-
-
+// Terraform statefiles are at terraform cloud
 terraform {
   required_providers {
     aws = {
@@ -32,11 +24,5 @@ terraform {
 module "s3-website" {
   source      = "./modules/s3-website"
   domain_name = var.site_name
-  # webpath     = "/Users/jag/Documents/CloudResumeChallenge/aws-crc/HTML-Webpage"
   webpath = "./HTML-Webpage"
 }
-
-
-# output "website_endpoint" {
-#   value = aws_s3_bucket.crc_web.website_endpoint
-# }
--- py_scripts/lambda4dynamodb.py
+++ py_scripts/lambda4dynamodb.py
@@ -10,25 +10,9 @@ def update_table(table, pk, column):
         UpdateExpression='ADD ' + column + ' :incr',
         ExpressionAttributeValues={':incr': 1}
     )
-    # new_counter = table.get_item(
-    #     Key={pk: 1}
-    # )
 
-
-    # print(new_counter['Item'][column])
     print(response)
-#lol
-# if name == 'main':
-#     update_table('test_Visitors', 'VisitorCount', 'vc')
 
-# def get_count(table, pk, column):
-#     dynamodb = boto3.resource('dynamodb')
-#     table = dynamo_client.Table(table)
-#     response = table.get_item(
-#             Key={pk: 1}
-#         )
-#     count = response['Item'][column]
-#     return(count)
 
 def get_count(table, pk, column):
     table = dynamo_client.Table(table)
