2ecdaf5c6c2193a62d5ecc8c81b9dae47c589e3a
jagan <jagan.mcc@gmail.com>
2023-03-03 22:41:28 -0500

    disabled R53 resources

--- acm.tf
+++ acm.tf
@@ -1,15 +1,15 @@
 // Create certificate 
-# resource "aws_acm_certificate" "cert_acm" {
-#   # provider = aws.us-east-1
-#   domain_name       = var.site_name
-#   validation_method = "DNS"
+resource "aws_acm_certificate" "cert_acm" {
+  # provider = aws.us-east-1
+  domain_name       = var.site_name
+  validation_method = "DNS"
 
-#   tags = local.tags
+  tags = local.tags
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
 # resource "aws_acm_certificate_validation" "acm_validate" {
 #   certificate_arn         = aws_acm_certificate.cert_acm.arn
